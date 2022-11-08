53b02b1d7c0b97dd8a48e8ad51158162016940f4
Adam Baker <joshbothhoth@gmail.com>
2022-11-07 21:17:08 -0600

    Pass key array as prop

--- src/components/UIElements/ScreenKeyboard.vue
+++ src/components/UIElements/ScreenKeyboard.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <KeyButton keyCode="KeyW" :player="player"/>
-    <KeyButton keyCode="KeyA" :player="player"/>
-    <KeyButton keyCode="KeyS" :player="player"/>
-    <KeyButton keyCode="KeyD" :player="player"/>
-    <KeyButton keyCode="Space" :player="player"/>
+    <KeyButton v-for="k in keys" :keyCode="k" :player="player"></KeyButton>
   </div>
 </template>
 
@@ -13,7 +9,7 @@ import KeyButton from '@/components/UIElements/KeyButton.vue'
 
 export default {
   components: { KeyButton },
-  props: ["player"]
+  props: ["player", "keys"],
 }
 
 </script>
