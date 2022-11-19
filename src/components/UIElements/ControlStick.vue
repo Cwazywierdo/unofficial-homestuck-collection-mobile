35e22703929150e0cc74266924303edfbef7a7c1
Adam Baker <joshbothhoth@gmail.com>
2022-11-18 22:12:32 -0600

    Implement mouse drag for stick

--- src/components/UIElements/ControlStick.vue
+++ src/components/UIElements/ControlStick.vue
@@ -1,25 +1,67 @@
 <template>
   <div class="control-stick" :style="styleProps">
-    <svg class="stick-area" :width="size" :height="size">
-      <circle :cx="size/2" :cy="size/2" :r="(size - strokeWidth) / 2" :stroke-width="strokeWidth" stroke="black" stroke-opacity="0.1" fill="none"/>
-      <polygon :points="octagonPoints" fill="black" fill-opacity="0.1" stroke="gray" :stroke-width="strokeWidth" />
+    <svg :width="size" :height="size">
+      <circle
+        :cx="size / 2"
+        :cy="size / 2"
+        :r="(size - strokeWidth) / 2"
+        :stroke-width="strokeWidth"
+        stroke="black"
+        stroke-opacity="0.1"
+        fill="none"
+      />
+      <polygon
+        :points="octagonPoints"
+        fill="black"
+        fill-opacity="0.1"
+        stroke="gray"
+        :stroke-width="strokeWidth"
+      />
+      <circle
+        :cx="size / 2"
+        :cy="size / 2"
+        :r="(size * thumbScalar - strokeWidth) / 2"
+        fill="gray"
+      />
     </svg>
-    <svg class="stick-thumb" :style="stickThunbStyleProps" :width="size * thumbScalar" :height="size * thumbScalar">
-      <circle :cx="size / 2 * thumbScalar" :cy="size / 2 * thumbScalar" :r="(size * thumbScalar - strokeWidth) / 2" :stroke-width="strokeWidth" stroke="green" stroke-opacity="0.5" fill="green"/>
+    <svg
+      class="stick-thumb"
+      :style="stickThunbStyleProps"
+      @mousedown="thumbMouseDown"
+      :width="size * thumbScalar"
+      :height="size * thumbScalar"
+    >
+      <circle
+        :cx="(size / 2) * thumbScalar"
+        :cy="(size / 2) * thumbScalar"
+        :r="(size * thumbScalar - strokeWidth) / 2"
+        :stroke-width="strokeWidth"
+        stroke="green"
+        stroke-opacity="1"
+        fill="lightgreen"
+      />
     </svg>
   </div>
 </template>
 
 <script>
-let x = 0, y = 0;
-
 export default {
   props: ["size"],
+  mounted() {
+    document.onmouseup = this.thumbMouseUp;
+    document.onmousemove = this.thumbMove;
+    this.stickThumb = this.$el.querySelector(".stick-thumb");
+    this.setThumbPos();
+  },
   data() {
     return {
       strokeWidth: 5,
-      thumbScalar: 0.3
-    }
+      thumbScalar: 0.4,
+      thumbX: 0,
+      thumbY: 0,
+      isClicked: false,
+      stickThumb: undefined
+    };
   },
   computed: {
     octagonPoints() {
@@ -28,9 +70,9 @@ export default {
       let retString = "";
 
       for (let i = 0; i < 8; i++) {
-        let cos = Math.cos(i / 4 * Math.PI);
-        let sin = Math.sin(i / 4 * Math.PI);
-        
+        let cos = Math.cos((i / 4) * Math.PI);
+        let sin = Math.sin((i / 4) * Math.PI);
+
         let px = center + cos * radius;
         let py = center + sin * radius;
 
@@ -40,20 +82,57 @@ export default {
     },
     stickThunbStyleProps() {
       return {
-        'left': `${(x+1) * this.size/2}px`,
-        'top': `${(y+1) * this.size/2}px`,
-        'margin-left': `${-this.size / 2 * this.thumbScalar}px`,
-        'margin-top': `${-this.size / 2 * this.thumbScalar}px`
-      }
+        "margin-left": `${(-this.size / 2) * this.thumbScalar}px`,
+        "margin-top": `${(-this.size / 2) * this.thumbScalar}px`
+      };
     },
     styleProps() {
       return {
-        'width': `${this.size}px`,
-        'height': `${this.size}px`
+        width: `${this.size}px`,
+        height: `${this.size}px`
+      };
+    }
+  },
+  methods: {
+    thumbMouseDown() {
+      this.isClicked = true;
+    },
+    thumbMouseUp() {
+      this.isClicked = false;
+      this.thumbX = 0;
+      this.thumbY = 0;
+    },
+    thumbMove(e) {
+      if (!this.isClicked) return;
+
+      let scalar = 2 / this.size;
+      this.thumbX += e.movementX * scalar;
+      this.thumbY += e.movementY * scalar;
+    },
+    setThumbPos() {
+      let realX = this.thumbX,
+        realY = this.thumbY;
+
+      let dist = realX * realX + realY * realY;
+      if (dist > 1) {
+        dist = Math.sqrt(dist);
+        realX /= dist;
+        realY /= dist;
       }
+
+      this.stickThumb.style.left = `${((realX + 1) * this.size) / 2}px`;
+      this.stickThumb.style.top = `${((realY + 1) * this.size) / 2}px`;
+    }
+  },
+  watch: {
+    thumbX() {
+      this.setThumbPos();
+    },
+    thumbY() {
+      this.setThumbPos();
     }
   }
-}
+};
 </script>
 
 <style scoped>
@@ -61,13 +140,10 @@ export default {
   display: inline-grid;
   margin: 10px;
   position: relative;
+  touch-action: none;
 }
 
-.stick-area {
-  position: absolute;
-}
-
-.stick-thumb {
+.control-stick > * {
   position: absolute;
 }
 </style>
